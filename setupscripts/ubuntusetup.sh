ce5288984ba68853396379ac8bcf0820487123f3
Ivy Bowman <ivanbowman05@gmail.com>
2022-03-12 00:56:48 -0500

    Remove chromedriver setup and old conf data

--- setupscripts/ubuntusetup.sh
+++
@@ -1,9 +0,0 @@
-LATEST_VERSION=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
-echo "Updating Packages"
-sudo apt-get update
-echo "Installing Prerequisites"
-sudo apt-get install -y unzip ffmpeg git python3-pip
-wget -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$LATEST_VERSION/chromedriver_linux64.zip
-echo "Installing ChromeDriver"
-sudo unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
-echo "Finished"
